63b1936a8257c4cf1e141d7b03f4dfc5eff23580
Prudhvi Suraaj <prudhvisuraaj@gmail.com>
2025-08-09 13:14:33 +0530

    initial commit 🚀

--- Dicee/ViewController.swift
+++ Dicee/ViewController.swift
@@ -8,12 +8,26 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var diceOne: UIImageView!
+    @IBOutlet weak var diceTwo: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func onRoll(_ sender: Any) {
+        let images : [UIImage] = [
+            UIImage(named: "DiceOne")!,
+            UIImage(named: "DiceTwo")!,
+            UIImage(named: "DiceThree")!,
+            UIImage(named: "DiceFour")!,
+            UIImage(named: "DiceFive")!,
+            UIImage(named: "DiceSix")!,
+        ];
+        // set random images to diceOne and diceTwo
+        diceOne.image = images.randomElement();
+        diceTwo.image = images.randomElement();
+    }
+    
 }
 
